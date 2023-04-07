8c968d2a0383a9a811ba8ace7529ef91fee8eb61
Jaryot <jarram999@gmail.com>
2023-04-07 16:49:44 -0700

    added lay out of readme under variable

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,133 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const readMeDoc = ({
+  projectTitle,
+  motivation,
+  purpose,
+  solve,
+  learn,
+  installation,
+  usage,
+  credits,
+  license,
+  gitname,
+  email,
+}) => {
+  return `
 
-// TODO: Create a function to initialize app
-function init() {}
+    # ${projectTitle}
 
-// Function call to initialize app
-init();
+    ## Description
+
+    ${motivation}
+    ${purpose}
+    ${solve}
+    ${learn}
+
+    ## Table of Contents
+    
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Credits](#credits)
+    -[License](#license)
+
+    ## Installation
+
+    ${installation}
+
+    ## Usage
+
+    ${usage}
+
+    ## Credit
+
+    ${credits}
+
+    ## License
+
+    This project uses the ${license} license. Please refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+    ## Questions
+
+    You can see my GitHub Profile at https://github.com/${gitname}.
+
+    You can reach me at my email: ${email}
+    `;
+};
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "project",
+      message: "What is your name?",
+    },
+    {
+      type: "checkbox",
+      message: "What languages do you know?",
+      name: "stack",
+      choices: ["HTML", "CSS", "JavaScript", "MySQL"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+    {
+      type: "list",
+      message: "What is your preferred method of communication?",
+      name: "contact",
+      choices: ["email", "phone", "telekinesis"],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
--- README.md
+++ README.md
@@ -61,3 +61,15 @@ If your project has a lot of features, list them here.
 ## How to Contribute
 
 If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+projectTitle,
+  motivation,
+  purpose,
+  solve,
+  learn,
+  installation,
+  usage,
+  credits,
+  license,
+  gitname,
+  email,
